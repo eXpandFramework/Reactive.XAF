847c84b0a373c571378e62830c4495fab0920283
apobekiaris <apostolis.bekiaris@gmail.com>
2019-06-13 00:35:20 +0300

    Update all versions due to VersionConverter

--- src/Modules/ModelMapper/Properties/AssemblyInfo.cs
+++ src/Modules/ModelMapper/Properties/AssemblyInfo.cs
@@ -28,8 +28,8 @@ using System.Runtime.InteropServices;
 //
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
-// [assembly: AssemblyVersion("1.1.23.0")]
-[assembly: AssemblyVersion("0.0.1.0")]
-[assembly: AssemblyFileVersion("0.0.1.0")]
+// [assembly: AssemblyVersion("1.1.24.0")]
+[assembly: AssemblyVersion("1.1.24.0")]
+[assembly: AssemblyFileVersion("1.1.24.0")]
 //[assembly:AllowPartiallyTrustedCallers]
 //[assembly: SecurityTransparent()]
