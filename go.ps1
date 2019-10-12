7d1bdaa9cd1483d3374eed67971448f39b91b10f
Apostolis Bekiaris <apostolis.bekiaris@gmail.com>
2019-10-12 10:55:19 +0300

    Release Logger packages

--- go.ps1
+++ go.ps1
@@ -8,7 +8,7 @@ param(
     [string]$branch="lab",
     [switch]$InstallModules,
     [string[]]$taskList=@("Release"),
-    [string]$XpandPwshVersion = "0.17.6",
+    [string]$XpandPwshVersion = "0.17.7",
     [switch]$Release
 )
 $ErrorActionPreference = "Stop"
