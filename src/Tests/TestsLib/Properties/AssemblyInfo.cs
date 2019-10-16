3e92c9837aa1a8e300c0016ec9050a0c20482cd3
Apostolis Bekiaris <apostolis.bekiaris@gmail.com>
2019-10-16 09:56:38 +0300

    Bump versions

--- src/Tests/TestsLib/Properties/AssemblyInfo.cs
+++ src/Tests/TestsLib/Properties/AssemblyInfo.cs
@@ -26,7 +26,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("Tests")]
-[assembly: AssemblyCopyright("Copyright eXpandFramework ©  2018")]
+[assembly: AssemblyCopyright("Copyright eXpandFramework ï¿½  2018")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -47,7 +47,7 @@ using System.Runtime.InteropServices;
 //
 // You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
-[assembly: AssemblyVersion("1.0.11.0")]
-[assembly: AssemblyFileVersion("1.0.11.0")]
+[assembly: AssemblyVersion("1.0.12.0")]
+[assembly: AssemblyFileVersion("1.0.12.0")]
 //[assembly:AllowPartiallyTrustedCallers]
 //[assembly: SecurityTransparent()]
