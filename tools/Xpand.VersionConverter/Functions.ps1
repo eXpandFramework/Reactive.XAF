e92bd7db083b96a357d4b91c38f24a3ab8b2f3f9
Apostolis Bekiaris <apostolis.bekiaris@gmail.com>
2019-10-22 07:05:58 +0300

    VersionConverter: Fails to detect DevExpress version #598

--- tools/Xpand.VersionConverter/Functions.ps1
+++ tools/Xpand.VersionConverter/Functions.ps1
@@ -223,7 +223,7 @@ function Get-DevExpressVersion($targetPath, $referenceFilter, $projectFile) {
         }
         else {
             $references = $csproj.Project.ItemGroup.Reference
-            $dxReferences = $references.Include | Where-Object { $_ -like "$referenceFilter" }    
+            $dxReferences = $references | Where-Object { $_.Include -like "$referenceFilter" }
             $hintPath = $dxReferences.HintPath | ForEach-Object { 
                 if ($_) {
                     $path = $_
