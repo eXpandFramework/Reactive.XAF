9e68bf5b3829559a1ac2609e9670fafcd570eb33
Apostolis Bekiaris <apostolis.bekiaris@gmail.com>
2019-12-01 13:41:51 +0200

    minor update

--- go.ps1
+++ go.ps1
@@ -9,7 +9,7 @@ param(
     [string]$branch="lab",
     [switch]$InstallModules,
     [string[]]$taskList=@("Build"),
-    [string]$XpandPwshVersion = "0.36.2",
+    [string]$XpandPwshVersion = "0.36.3",
     [switch]$Debug,
     [switch]$CustomVersion
 )
