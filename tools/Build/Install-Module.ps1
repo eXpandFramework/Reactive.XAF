57a9c473ad15f456d0da19c320fa988e58bc7c1b
Apostolis Bekiaris <apostolis.bekiaris@gmail.com>
2019-12-01 06:24:07 +0200

    minor update

--- tools/Build/Install-Module.ps1
+++ tools/Build/Install-Module.ps1
@@ -5,7 +5,6 @@ $module=Get-module $psObj.Name -ListAvailable|Where-Object{$_.Version -eq $psObj
 if (!$module){
     $name=$psObj.Name
     $version=$psObj.Version
-    Write-Warning "-> Installing $name $version"
     Install-Module $name -RequiredVersion $version -Scope CurrentUser -AllowClobber -Force
 }
 Import-Module $psObj.Name -Global -Prefix X -RequiredVersion $psObj.Version -Force 
