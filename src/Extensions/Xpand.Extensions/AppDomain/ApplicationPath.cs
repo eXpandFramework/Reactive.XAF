3a64f9bf23d145169a8bbf167f7f165257f893f4
Apostolis Bekiaris <apostolis.bekiaris@gmail.com>
2020-02-19 18:13:24 +0200

    minor update

--- src/Extensions/Xpand.Extensions/AppDomain/ApplicationPath.cs
+++ src/Extensions/Xpand.Extensions/AppDomain/ApplicationPath.cs
@@ -1,6 +1,8 @@
 ﻿using Fasterflect;
+using JetBrains.Annotations;
 
 namespace Xpand.Extensions.AppDomain{
+    [PublicAPI]
     public static partial class AppDomainExtensions{
         public static string ApplicationPath(this global::System.AppDomain appDomain){
             if (System.Runtime.InteropServices.RuntimeInformation.FrameworkDescription.StartsWith(".NET Framework")){
