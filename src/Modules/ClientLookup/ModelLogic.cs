0c1222c087648c9973db56555f923edeea7edf82
Apostolis Bekiaris <apostolis.bekiaris@gmail.com>
2020-02-28 15:22:40 +0200

    init

---
+++ src/Modules/ClientLookup/ModelLogic.cs
@@ -0,0 +1,111 @@
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Diagnostics;
+using System.Drawing;
+using System.Linq;
+using System.Reactive.Linq;
+using DevExpress.ExpressApp;
+using DevExpress.ExpressApp.ConditionalAppearance;
+using DevExpress.ExpressApp.Model;
+using DevExpress.ExpressApp.Model.Core;
+using Xpand.Extensions.Reactive.Filter;
+using Xpand.Extensions.XAF.Model;
+
+
+namespace Xpand.XAF.Modules.Reactive.Logger{
+    public interface IModelReactiveModuleLogger:IModelReactiveModule{
+        IModelReactiveLogger ReactiveLogger{ get; }
+    }
+
+    public static class ModelReactiveModuleLogger{
+        public static IObservable<IModelReactiveLogger> ReactiveLogger(this IObservable<IModelReactiveModules> source){
+            return source.Select(modules => modules.ReactiveLogger()).WhenNotDefault();
+        }
+        public static IModelReactiveLogger ReactiveLogger(this IModelReactiveModules reactiveModules){
+            var modelReactiveModuleLogger = reactiveModules as IModelReactiveModuleLogger;
+            return modelReactiveModuleLogger?.ReactiveLogger;
+        }
+    }
+    public interface IModelReactiveLogger:IModelNode{
+        IModelTraceSourcedModules TraceSources{ get; }
+    }
+
+    public static class ModelReactiveLoggerService{
+        public static IEnumerable<IModelTraceSourcedModule> GetActiveSources(this IModelReactiveLogger logger){
+            return logger.TraceSources.Enabled
+                ? new CalculatedModelNodeList<IModelTraceSourcedModule>( logger.TraceSources.Where(_ => _.Level!=SourceLevels.Off))
+                : new CalculatedModelNodeList<IModelTraceSourcedModule>(Enumerable.Empty<IModelTraceSourcedModule>());
+        }
+
+    }
+    [ModelNodesGenerator(typeof(TraceSourcedModulesNodesGenerator))]
+    public interface IModelTraceSourcedModules:IModelNode,IModelList<IModelTraceSourcedModule>{
+        [DefaultValue(false)]
+        bool Enabled{ get; set; }
+    }
+
+    public class TraceEventAppearenceRulesGenerator:ModelNodesGeneratorUpdater<AppearanceRulesModelNodesGenerator>{
+        public static readonly Dictionary<Color, string> Modules=new Dictionary<Color, string>{
+            {Color.Black, nameof(ReactiveModule)},
+            {Color.DimGray, nameof(ReactiveLoggerModule)},
+            {Color.DarkOrange, "ReactiveLoggerHubModule"},
+            {Color.Blue, "AutoCommitModule"},
+            {Color.BlueViolet, "CloneMemberValueModule"},
+            {Color.Brown, "CloneModelViewModule"},
+            {Color.BurlyWood, "GridListEditorModule"},
+            {Color.CadetBlue, "HideToolBarModule"},
+            {Color.Chartreuse, "MasterDetailModule"},
+            {Color.Chocolate, "ModelMapperModule"},
+            {Color.DarkGoldenrod, "ModelViewInheritanceModule"},
+            {Color.DarkGray, "OneViewModule"},
+            {Color.DarkGreen, "ProgressBarViewItemModule"},
+            {Color.DarkKhaki, "RefreshViewModule"},
+            {Color.DarkMagenta, "SuppressConfirmationModule"},
+            {Color.DarkRed, "ViewEditModule"}
+        };
+
+        public override void UpdateNode(ModelNode node){
+            if (node.GetParent<IModelClass>().TypeInfo.Type==typeof(TraceEvent)){
+                foreach (var module in Modules){
+                    var modelAppearanceRule = node.AddNode<IModelAppearanceRule>($"{module.Value}Source");
+                    modelAppearanceRule.TargetItems = nameof(TraceEvent.Source);
+                    modelAppearanceRule.FontColor=module.Key;
+                    modelAppearanceRule.Context = "ListView";
+                    modelAppearanceRule.Criteria = "[" + nameof(TraceEvent.Source) + "] = '" + module.Value + "'";
+                }
+            }
+        }
+    }
+    public class TraceSourcedModulesNodesGenerator:ModelNodesGeneratorBase{
+        protected override void GenerateNodesCore(ModelNode node){
+            foreach (var module in TraceEventAppearenceRulesGenerator.Modules){
+                node.AddNode<IModelTraceSourcedModule>(module.Value);
+            }
+            var modules = TraceEventAppearenceRulesGenerator.Modules
+                .SelectMany(_ => ((IModelSources) node.Application).Modules.Where(m => m.Name==_.Value).ToTraceSource());
+            foreach (var valueTuple in modules){
+                var moduleName = valueTuple.module.Name;
+                AddTraceSource(node, moduleName, valueTuple);
+            }
+
+        }
+
+        private static void AddTraceSource(ModelNode node, string moduleName,
+            (ModuleBase module, TraceSource traceSource) valueTuple){
+            if (node[moduleName]==null){
+                var sourcedModule = node.AddNode<IModelTraceSourcedModule>(moduleName);
+                sourcedModule.Level = valueTuple.traceSource.Switch.Level;
+            }
+        }
+    }
+
+    public interface IModelTraceSourcedModule:IModelNode{
+        [DefaultValue(SourceLevels.Verbose)]
+        SourceLevels Level{ get; set; }
+        
+    }
+
+    
+    
+}
---
+++ src/Modules/ClientLookup/Module.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using DevExpress.ExpressApp;
+using DevExpress.ExpressApp.Model;
+using DevExpress.ExpressApp.Model.Core;
+using Xpand.Extensions.Reactive.Conditional;
+using Xpand.XAF.Modules.Reactive.Extensions;
+
+namespace Xpand.XAF.Modules.Reactive.Logger {
+    public sealed class ReactiveLoggerModule : ReactiveModuleBase{
+        public const string CategoryName = "Xpand.XAF.Modules.Reactive.Logger";
+
+        static ReactiveLoggerModule(){
+            TraceSource=new ReactiveTraceSource(nameof(ReactiveLoggerModule));
+        }
+        public ReactiveLoggerModule() {
+            RequiredModuleTypes.Add(typeof(DevExpress.ExpressApp.SystemModule.SystemModule));
+            RequiredModuleTypes.Add(typeof(DevExpress.ExpressApp.ConditionalAppearance.ConditionalAppearanceModule));
+            RequiredModuleTypes.Add(typeof(ReactiveModule));
+        }
+
+        public static ReactiveTraceSource TraceSource{ get; set; }
+        public override void Setup(ApplicationModulesManager moduleManager){
+            base.Setup(moduleManager);
+        }
+
+        public override void AddGeneratorUpdaters(ModelNodesGeneratorUpdaters updaters){
+            base.AddGeneratorUpdaters(updaters);
+            updaters.Add(new TraceEventAppearenceRulesGenerator());
+        }
+
+        public override void Setup(XafApplication application){
+            base.Setup(application);
+            
+            this.Connect()
+                .TakeUntilDisposed(this)
+                .Subscribe();
+        }
+
+        public override void ExtendModelInterfaces(ModelInterfaceExtenders extenders){
+            base.ExtendModelInterfaces(extenders);
+            
+            extenders.Add<IModelReactiveModules,IModelReactiveModuleLogger>();
+            
+        }
+    }
+
+}
