1e42580eb99b929ac2c8c23e350463d867e509ae
apobekiaris <apostolis.bekiaris@gmail.com>
2020-06-25 11:56:07 +0300

    Add Target, platform to modules readme

--- Build/UpdateReadMe.ps1
+++ Build/UpdateReadMe.ps1
@@ -35,13 +35,33 @@ function GetModuleName($_){
     $moduleName
 }
 function UpdateModulesList($rootLocation, $packages) {
-    $moduleList = "|PackageName|Version|[![Custom badge](https://xpandshields.azurewebsites.net/endpoint.svg?label=Nuget.org&url=https%3A%2F%2Fxpandnugetstats.azurewebsites.net%2Fapi%2Ftotals%2FXAF)](https://www.nuget.org/packages?q=Xpand.XAF)`r`n|---|---|---|`r`n"
+    $moduleList = "|PackageName|Version|[![Custom badge](https://xpandshields.azurewebsites.net/endpoint.svg?label=&url=https%3A%2F%2Fxpandnugetstats.azurewebsites.net%2Fapi%2Ftotals%2FXAF)](https://www.nuget.org/packages?q=Xpand.XAF)|Target|Platform`r`n|---|---|---|---|---|`r`n"
+    $projects=Get-MSBuildProjects $rootLocation\src\Modules
+    $assemblies=Get-ChildItem $rootLocation\bin xpand.*.dll
     $packages | ForEach-Object {
         $name = $_.Replace("Xpand.XAF.Modules.", "")
         $packageUri = "[$name](https://github.com/eXpandFramework/DevExpress.XAF/tree/master/src/Modules/$name)"
         $version = "![](https://xpandshields.azurewebsites.net/nuget/v/$_.svg?label=&style=flat)"
         $downloads = "![](https://xpandshields.azurewebsites.net/nuget/dt/$_.svg?label=&style=flat)"
-        $moduleList += "$packageUri|$version|$downloads`r`n"
+        $targetFramework =$null
+        if ($_ -like "*.All"){
+            $regex = [regex] '(?is)Xpand\.XAF\.(?<platform>[^\.]*)\.All'
+            $result = $regex.Match($_).Groups['platform'].Value;
+            $platform="Agnostic"
+            if ($result -ne "Core"){
+                $platform=$result
+            }
+        }else{
+            $packageName=$_
+            $project=$projects|Where-Object{$_.BaseName -eq $packageName}|Get-XmlContent
+            $targetFramework =Get-ProjectTargetFramework $project -FullName
+            $platform=($assemblies|Where-Object{$_.BaseName -eq $packageName}|Get-AssemblyMetadata -key Platform).Value
+            if ($platform -eq "Core"){
+                $platform="Agnostic"
+            }
+        }
+        
+        $moduleList += "$packageUri|$version|$downloads|$targetFramework|$platform`r`n"
     }
     
     $path = "$rootLocation\src\modules\ReadMe.md"
