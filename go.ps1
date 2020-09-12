42aab393983cfe13ef35ef67766a54c71c68ffd0
apobekiaris <apostolis.bekiaris@gmail.com>
2020-09-12 10:45:22 +0300

    bump versions

--- go.ps1
+++ go.ps1
@@ -9,7 +9,7 @@ param(
     [string]$branch="lab",
     [switch]$InstallModules,
     [string[]]$taskList=@("Build"),
-    [string]$XpandPwshVersion = "1.202.40.36",
+    [string]$XpandPwshVersion = "1.202.40.38",
     [string[]]$ChangedModules=@(),
     [switch]$CustomVersion
 )
