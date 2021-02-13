8cb7459dd47b6177b48688b536f88a9acbfd50e2
apobekiaris <apostolis.bekiaris@gmail.com>
2021-02-13 17:56:26 +0200

    4.202.51.0

--- Build/UpdateDependencies.ps1
+++ Build/UpdateDependencies.ps1
@@ -2,7 +2,10 @@ param(
     [version]$CustomVersion=19.2.10.0
 )
 
-if ($CustomVersion -lt "20.1.8"){
-    Remove-ContentLine (Get-PaketDependenciesPath) "DevExpress.ExpressApp.Blazor.All"
+if ($CustomVersion -lt "20.2.0"){
+    "DevExpress.ExpressApp.EasyTest.BlazorAdapter","DevExpress.ExpressApp.Blazor.All"|ForEach-Object{
+        Remove-ContentLine (Get-PaketDependenciesPath) $_
+    }
+    
     Remove-ContentLine "$PSScriptRoot\..\src\Extensions\Xpand.Extensions.Office.Cloud.Google.Blazor\paket.references" "DevExpress.ExpressApp.Blazor"
 }
--- src/Common/AssemblyInfoVersion.cs
+++ src/Common/AssemblyInfoVersion.cs
@@ -1,1 +1,1 @@
-class AssemblyInfoVersion {     public const string Version = "4.202.50.2"; }
+class AssemblyInfoVersion {     public const string Version = "4.202.51.0"; }
