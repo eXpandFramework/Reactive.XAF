22e276e5769c0035dcb2f3964780f4e4f2f6017a
apobekiaris <apostolis.bekiaris@gmail.com>
2022-07-25 23:55:13 +0300

    XpandModelEditor for Rider 22.1.2 #942

--- tools/Xpand.XAF.ModelEditor/IDE/Rider/src/rider/main/kotlin/com/jetbrains/rider/plugins/sampleplugin/XpandModelEditorAction.kt
+++
@@ -1,8 +0,0 @@
-package com.jetbrains.rider.plugins.xpand
-
-import com.jetbrains.rider.actions.base.RiderAnAction
-
-class SampleAction : RiderAnAction(
-        "SampleAction",
-        "Do Something1",
-        "Do Something Description")
