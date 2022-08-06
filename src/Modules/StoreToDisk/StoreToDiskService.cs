d389f45ede9547d4ec3bc7f63bbadcbc4144b55d
apobekiaris <apostolis.bekiaris@gmail.com>
2022-08-06 23:01:24 +0300

    StoreToDisk: deserialization fails when files not exist and Updater

--- src/Modules/StoreToDisk/StoreToDiskService.cs
+++ src/Modules/StoreToDisk/StoreToDiskService.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Diagnostics.CodeAnalysis;
 using System.IO;
 using System.Linq;
@@ -14,7 +15,6 @@ using Xpand.Extensions.BytesExtensions;
 using Xpand.Extensions.JsonExtensions;
 using Xpand.Extensions.LinqExtensions;
 using Xpand.Extensions.ObjectExtensions;
-using Xpand.Extensions.Reactive.Combine;
 using Xpand.Extensions.Reactive.Filter;
 using Xpand.Extensions.Reactive.Transform;
 using Xpand.Extensions.Reactive.Transform.System.IO;
@@ -32,42 +32,7 @@ namespace Xpand.XAF.Modules.StoreToDisk{
             => manager.WhenApplication(application => application.WhenSetupComplete()
                 .SelectMany(_ =>application.StoreToDisk(application.Model.ToReactiveModule<IModelReactiveModulesStoreToDisk>().StoreToDisk.Folder) )
                 .ToUnit());
-        
-        static IObservable<(IObjectSpace objectSpace, (object instance, ObjectModification modification)[] details)> WhenProviderCommittingDetailed1(
-            this XafApplication application,Type objectType,ObjectModification objectModification,Func<object,bool> criteria=null,bool emitUpdatingObjectSpace=false,params string[] modifiedProperties)
-            => application.WhenProviderObjectSpaceCreated(emitUpdatingObjectSpace).WhenCommittingDetailed1(objectType,objectModification, criteria,modifiedProperties);
-        static IObservable<(IObjectSpace objectSpace, (object instance, ObjectModification modification)[] details)> WhenCommittingDetailed1(this IObservable<IObjectSpace> source,
-            Type objectType,ObjectModification objectModification,Func<object,bool> criteria,params string[] modifiedProperties)
-            => source.SelectMany(objectSpace => 
-                objectSpace.WhenCommitingDetailed1(objectType,objectModification,false,criteria,modifiedProperties).TakeUntil(objectSpace.WhenDisposed()));
-        
-        static IObservable<(IObjectSpace objectSpace, (object instance, ObjectModification modification)[] details)>
-            WhenCommitingDetailed1(this IObjectSpace objectSpace,Type objectType, ObjectModification objectModification, bool emitAfterCommit,Func<object,bool> criteria=null,
-                params string[] modifiedProperties) 
-            => objectSpace.WhenCommitingDetailed1(objectType, emitAfterCommit, objectModification,criteria,modifiedProperties);
 
-        static IObservable<(IObjectSpace objectSpace, (object instance, ObjectModification modification)[] details)> WhenCommitingDetailed1(
-            this IObjectSpace objectSpace,Type objectType, bool emitAfterCommit, ObjectModification objectModification,Func<object,bool> criteria=null, params string[] modifiedProperties) 
-            => objectSpace.WhenModifiedObjects1(objectType,modifiedProperties)
-                .Select(_ => objectSpace.WhenCommiting()
-                    .SelectMany(_ => {
-                        var modifiedObjects = objectSpace.ModifiedObjects(objectType, objectModification)
-                            .Where(t => criteria==null|| criteria.Invoke(t.instance)).ToArray();
-                        return modifiedObjects.Any() ? emitAfterCommit ? objectSpace.WhenCommitted().FirstAsync().Select(space => (space, modifiedObjects))
-                            : (objectSpace, modifiedObjects).ReturnObservable() : Observable.Empty<(IObjectSpace, (object instance, ObjectModification modification)[])>();
-                    })).Switch();
-        private static bool PropertiesMatch(this string[] properties, (IObjectSpace objectSpace, ObjectChangedEventArgs e) t) 
-            => !properties.Any()||(t.e.MemberInfo != null && properties.Contains(t.e.MemberInfo.Name) ||
-                                   t.e.PropertyName != null && properties.Contains(t.e.PropertyName));
-
-        static IObservable<object> WhenModifiedObjects1(this IObjectSpace objectSpace,Type objectType, params string[] properties) 
-            => Observable.Defer(() => objectSpace.WhenObjectChanged().Where(t => objectType.IsInstanceOfType(t.e.Object) && properties.PropertiesMatch( t))
-                    .Select(_ => _.e.Object).Take(1))
-                .RepeatWhen(observable => observable.SelectMany(_ => objectSpace.WhenModifyChanged().Where(space => !space.IsModified).Take(1)))
-                .TakeUntil(objectSpace.WhenDisposed());
-
-
-        
         private static IObservable<(object[] objects, JArray jArray)> LoadFromDisk(this object[] objects, XafApplication application,
             IMemberInfo keyMember, IMemberInfo[] memberInfos, ITypeInfo typeInfo, string filePath, StoreToDiskAttribute attribute) 
             => Observable.Defer(() => new FileInfo(filePath).WhenFileReadAsBytes()
@@ -86,14 +51,23 @@ namespace Xpand.XAF.Modules.StoreToDisk{
         
         public static IObservable<Unit> StoreToDisk(this XafApplication application, string directory)
             => application.StoreToDiskData(directory)
-                .SelectMany(t => application.WhenProviderCommittedDetailed(t.typeInfo.Type,ObjectModification.NewOrUpdated,emitUpdatingObjectSpace:true)
-                    .SelectMany(t1 => t1.details.Where(t2 =>t2.modification==ObjectModification.New ).Select(t3 => t3.instance).ToArray()
-                        .LoadFromDisk(application, t.keyMember, t.memberInfos, t.typeInfo, t.filePath, t.attribute)
-                        .SelectMany(t2 => (t2.objects.Concat(t1.details
-                            .Where(t3 => t3.modification == ObjectModification.Updated)
-                            .Select(t4 => t4.instance).ToArray()).ToArray(),t2.jArray).StoreToDisk(t.keyMember, t.memberInfos, t.filePath, t.attribute))))
+                .SelectMany(data => application.WhenProviderCommittedDetailed(data.typeInfo.Type,ObjectModification.NewOrUpdated,emitUpdatingObjectSpace:true)
+                    .SelectMany(committed => committed.details.Where(details =>details.modification==ObjectModification.New ).Select(newObjects => newObjects.instance).ToArray()
+                        .LoadFromDisk(application, data.keyMember, data.memberInfos, data.typeInfo, data.filePath, data.attribute)
+                        .StoreToDisk(committed,data)))
                 .ToUnit();
         
+        private static IObservable<JObject[]> StoreToDisk(this IObservable<(object[] objects, JArray jArray)> source,
+                (IObjectSpace objectSpace, (object instance, ObjectModification modification)[] details) committed,
+                (IMemberInfo keyMember, IMemberInfo[] memberInfos, ITypeInfo typeInfo, string filePath, StoreToDiskAttribute attribute) data)
+            => source.SelectMany(loadFromDisk => data.ObjectsToStore(committed,  loadFromDisk.objects,loadFromDisk.objects.Select(o => data.keyMember.GetValue(o)).ToHashSet())
+                .StoreToDisk(loadFromDisk.jArray,data.keyMember, data.memberInfos, data.filePath, data.attribute));
+
+        private static object[] ObjectsToStore(this (IMemberInfo keyMember, IMemberInfo[] memberInfos, ITypeInfo typeInfo, string filePath, StoreToDiskAttribute attribute) data,
+            (IObjectSpace objectSpace, (object instance, ObjectModification modification)[] details) committed, object [] loadedObjects, HashSet<object> loadedKeys) 
+            => committed.details.Where(details => !loadedKeys.Contains(data.keyMember.GetValue(details.instance)))
+                .Select(updated => updated.instance).Concat(loadedObjects).ToArray();
+
         private static IObservable<(IMemberInfo keyMember, IMemberInfo[] memberInfos, ITypeInfo typeInfo, string filePath, StoreToDiskAttribute attribute)> StoreToDiskData(
                 this XafApplication application, string directory)
             => application.TypesInfo.PersistentTypes.Attributed<StoreToDiskAttribute>().ToNowObservable()
@@ -101,21 +75,20 @@ namespace Xpand.XAF.Modules.StoreToDisk{
                 .SelectMany(t => new DirectoryInfo(directory).WhenDirectory().Select(_ => t.typeInfo.EnsureFile(directory))
                     .Select(filePath => ( t.keyMember, t.memberInfos, t.typeInfo, filePath,t.attribute)));
         
-        
-        private static IObservable<JObject[]> StoreToDisk(this (object[] objects, JArray jArray) source,IMemberInfo keyMember, IMemberInfo[] memberInfos, string filePath, StoreToDiskAttribute attribute) 
-            => source.objects.ToNowObservable().SelectMany(instance => {
-                var jtoken = source.jArray.GetToken(keyMember, memberInfos,  instance);
+        private static IObservable<JObject[]> StoreToDisk(this object[] objects ,JArray jArray,IMemberInfo keyMember, IMemberInfo[] memberInfos, string filePath, StoreToDiskAttribute attribute) 
+            => objects.ToNowObservable().SelectMany(instance => {
+                var jtoken = jArray.GetToken(keyMember, memberInfos,  instance);
                 return memberInfos.ToNowObservable().Do(memberInfo =>
                         jtoken[memberInfo.Name] = memberInfo.GetValue(instance).ToJToken())
                     .ConcatIgnoredValue(jtoken);
             }).BufferUntilCompleted(true)
-                .Do(objects => attribute.SaveFile(filePath,  new JArray(objects.Concat(source.ReplaceExisting(keyMember))).ToString()))
+                .Do(jObjects => attribute.SaveFile(filePath,  new JArray(jObjects.Concat(objects.ReplaceExisting(jArray, keyMember))).ToString()))
                 .TraceStoreToDisk();
 
-        private static JArray ReplaceExisting(this (object[] objects, JArray jArray) source ,IMemberInfo keyMember) {
-            source.objects.ForEach(o => source.jArray.Where(token => keyMember.Match(token, o)).Take(1).ToArray()
-                    .ForEach(token => source.jArray.Remove(token)));
-            return source.jArray;
+        private static JArray ReplaceExisting(this object[] objects,JArray jArray  ,IMemberInfo keyMember) {
+            objects.ForEach(o => jArray.Where(token => keyMember.Match(token, o)).Take(1).ToArray()
+                    .ForEach(token => jArray.Remove(token)));
+            return jArray;
         }
 
         [SuppressMessage("Interoperability", "CA1416:Validate platform compatibility")]
