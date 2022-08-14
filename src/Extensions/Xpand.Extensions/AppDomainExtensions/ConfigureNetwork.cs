df0993770f3dd8b254fa76661329fd8987a082ee
apobekiaris <apostolis.bekiaris@gmail.com>
2022-08-14 21:16:33 +0300

    ConfigureNetwork, FastUpdateGridView

---
+++ src/Extensions/Xpand.Extensions/AppDomainExtensions/ConfigureNetwork.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Net;
+using System.Threading;
+
+namespace Xpand.Extensions.AppDomainExtensions {
+    public static partial class AppDomainExtensions {
+        public static void ConfigureNetwork(this AppDomain appDomain, int connectionLimit = 100, int workerThreads = 100, int asyncThreads = 4) {
+            ServicePointManager.SecurityProtocol = SecurityProtocolType.Tls | SecurityProtocolType.Tls12 | SecurityProtocolType.Tls11;
+            ServicePointManager.DefaultConnectionLimit = connectionLimit;
+            ThreadPool.SetMinThreads(workerThreads, asyncThreads);
+        }
+    }
+}
--- src/Modules/Reactive/Services/ListEditorExtensions.cs
+++ src/Modules/Reactive/Services/ListEditorExtensions.cs
@@ -1,11 +1,24 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Reactive.Concurrency;
 using System.Reactive.Linq;
 using DevExpress.ExpressApp.Editors;
+using Fasterflect;
 using Xpand.Extensions.Reactive.Transform;
+using Xpand.Extensions.Reactive.Utility;
 
 namespace Xpand.XAF.Modules.Reactive.Services{
-    public static class ListEditorExtensions{
+    public static class ListEditorExtensions {
+        public static IObservable<T> UpdateGridView<T>(this ListEditor editor, Func<IEnumerable<T>> selector)
+            => editor.UpdateGridView(() => selector().ToNowObservable());
+        
+        public static IObservable<T> UpdateGridView<T>(this ListEditor editor,Func<IObservable<T>> selector) 
+            => editor.Defer(() => {
+                editor.GetPropertyValue("GridView").CallMethod("BeginDataUpdate");
+                return selector();
+
+            }).Finally(() => editor.GetPropertyValue("GridView").CallMethod("EndDataUpdate"));
+
         public static IObservable<(ListEditor sender, EventArgs e)> WhenModelApplied(this ListEditor editor) 
             => Observable.FromEventPattern<EventHandler<EventArgs>, EventArgs>(
                     handler => editor.ModelApplied += handler,
