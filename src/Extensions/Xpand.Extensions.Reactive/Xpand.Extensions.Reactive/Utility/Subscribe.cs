b1f18594f1bd29bd2093553e51bc069a9cc949d1
apobekiaris <apostolis.bekiaris@gmail.com>
2022-10-19 09:39:39 +0300

    move to another file

--- src/Extensions/Xpand.Extensions.Reactive/Xpand.Extensions.Reactive/Utility/Subscribe.cs
+++ src/Extensions/Xpand.Extensions.Reactive/Xpand.Extensions.Reactive/Utility/Subscribe.cs
@@ -1,9 +1,13 @@
 ﻿using System;
 using System.Reactive.Concurrency;
+using System.Reactive.Disposables;
 using System.Reactive.Linq;
 
 namespace Xpand.Extensions.Reactive.Utility{
     public static partial class Utility {
+        public static IObservable<T> OnUnsubscribe<T>(this IObservable<T> source, Action unsubscribe) 
+            => Observable.Create<T>(o => new CompositeDisposable(source.Subscribe(o), Disposable.Create(unsubscribe)));
+        
         public static IObservable<T> ReplayConnect<T>(this IObservable<T> source, IScheduler scheduler,
             int bufferSize = 0)
             => source.SubscribeReplay(scheduler, bufferSize);
@@ -17,5 +21,17 @@ namespace Xpand.Extensions.Reactive.Utility{
         }
         public static IObservable<T> ReplayConnect<T>(this IObservable<T> source, int bufferSize = 0) 
             => source.SubscribeReplay(bufferSize);
+        
+        public static IObservable<T> SubscribeOnDefault<T>(this IObservable<T> source)
+            => source.SubscribeOn(DefaultScheduler.Instance);
+        
+        public static IObservable<T> PublishConnect<T>(this IObservable<T> source) 
+            => source.SubscribePublish();
+
+        public static IObservable<T> SubscribePublish<T>(this IObservable<T> source){
+            var publish = source.Publish();
+            publish.Connect();
+            return publish;
+        }
     }
 }
