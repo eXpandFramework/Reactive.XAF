05dac68aff8376fbda99b23b285ca0c440e7e582
apobekiaris <apostolis.bekiaris@gmail.com>
2024-03-23 03:49:49 +0200

    Enclose string extension

---
+++ src/Extensions/Xpand.Extensions/StringExtensions/Enclose.cs
@@ -0,0 +1,12 @@
+﻿using Xpand.Extensions.LinqExtensions;
+
+namespace Xpand.Extensions.StringExtensions {
+    public static partial class StringExtensions {
+        public static string Enclose(this string input, string start, string end = null) {
+            if (input == null)
+                return null;
+            end ??= start;
+            return input.StartsWith(start) && input.EndsWith(end) ? input : new[] { start, input, end }.Join();
+        }
+    }
+}
