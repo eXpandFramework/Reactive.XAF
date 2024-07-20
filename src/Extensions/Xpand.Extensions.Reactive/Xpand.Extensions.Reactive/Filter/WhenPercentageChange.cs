e68b40424b5e0e91c5a0a3a81fd8753d67a2d6cb
apobekiaris <apostolis.bekiaris@gmail.com>
2024-07-20 07:09:19 +0300

    WhenPercentageChange

---
+++ src/Extensions/Xpand.Extensions.Reactive/Xpand.Extensions.Reactive/Filter/WhenPercentageChange.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Reactive.Linq;
+
+namespace Xpand.Extensions.Reactive.Filter {
+    public static partial class Filter {
+        public static IObservable<decimal> WhenPercentageChange(this IObservable<decimal> source, decimal percentageThreshold) 
+            => source.Scan((lastEmitted: (decimal?)null, toEmit: (decimal?)null), (acc, current) => acc.lastEmitted == null
+                ? (current, current) : Math.Abs((current - acc.lastEmitted.Value) / acc.lastEmitted.Value) > percentageThreshold
+                    ? (current, current) : (acc.lastEmitted, null)).Where(result => result.toEmit.HasValue)
+                .Select(result => result.toEmit!.Value);
+    }
+}
