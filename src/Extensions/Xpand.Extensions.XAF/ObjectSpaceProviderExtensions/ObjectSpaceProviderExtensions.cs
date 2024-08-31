76c350ef8c3edef340fbdef5f2434c525e8bf7e4
apobekiaris <apostolis.bekiaris@gmail.com>
2024-08-31 15:57:54 +0300

    SequenceGenerator: throws when cache enabled #1069

--- src/Extensions/Xpand.Extensions.XAF/ObjectSpaceProviderExtensions/ObjectSpaceProviderExtensions.cs
+++ src/Extensions/Xpand.Extensions.XAF/ObjectSpaceProviderExtensions/ObjectSpaceProviderExtensions.cs
@@ -1,4 +1,5 @@
 ﻿using DevExpress.ExpressApp;
+using DevExpress.Xpo.DB.Helpers;
 using Fasterflect;
 
 using Xpand.Extensions.ObjectExtensions;
@@ -9,6 +10,11 @@ namespace Xpand.Extensions.XAF.ObjectSpaceProviderExtensions{
             var connection = objectSpaceProvider.GetPropertyValue("DataLayer")?.GetPropertyValue("Connection");
             return (string) (connection != null ? connection.GetPropertyValue("ConnectionString")
                 : objectSpaceProvider.GetPropertyValue("DataStoreProvider").GetPropertyValue("ConnectionString"));
+        }
+        public static string GetDataBase(this IObjectSpaceProvider objectSpaceProvider) {
+            var parser = new ConnectionStringParser(objectSpaceProvider.GetConnectionString());
+            return parser.GetPartByName("Initial Catalog");
+
         }
 
         public static bool IsMiddleTier(this IObjectSpaceProvider objectSpaceProvider)
