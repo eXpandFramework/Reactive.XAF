26256decbf9a82ae1e1e76c907d25af5ee09cd4a
apobekiaris <apostolis.bekiaris@gmail.com>
2024-09-02 21:58:54 +0300

    TimeSpanNotEmptyAttribute

---
+++ src/Extensions/Xpand.Extensions.XAF/Attributes/Validation/TimeSpanNotZeroAttribute.cs
@@ -0,0 +1,8 @@
+﻿using System;
+using DevExpress.Persistent.Validation;
+
+namespace Xpand.Extensions.XAF.Attributes.Validation {
+    [AttributeUsage(AttributeTargets.Property)]
+    public class TimeSpanNotZeroAttribute() : RuleValueComparisonAttribute("TimeSpanNotEmpty", DefaultContexts.Save,
+        ValueComparisonType.NotEquals, "#00:00:00#", ParametersMode.Expression);
+}
