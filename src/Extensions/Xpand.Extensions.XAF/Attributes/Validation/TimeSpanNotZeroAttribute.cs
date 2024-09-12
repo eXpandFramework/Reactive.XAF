5ed697790a90f12d2a40eb11897a075ad39ab914
apobekiaris <apostolis.bekiaris@gmail.com>
2024-09-12 13:05:37 +0300

    TimeSpanNotZeroAttribute: autogenerate id

--- src/Extensions/Xpand.Extensions.XAF/Attributes/Validation/TimeSpanNotZeroAttribute.cs
+++ src/Extensions/Xpand.Extensions.XAF/Attributes/Validation/TimeSpanNotZeroAttribute.cs
@@ -1,8 +1,14 @@
 ﻿using System;
+using System.IO;
+using System.Runtime.CompilerServices;
 using DevExpress.Persistent.Validation;
 
 namespace Xpand.Extensions.XAF.Attributes.Validation {
     [AttributeUsage(AttributeTargets.Property)]
-    public class TimeSpanNotZeroAttribute() : RuleValueComparisonAttribute("TimeSpanNotEmpty", DefaultContexts.Save,
-        ValueComparisonType.NotEquals, "#00:00:00#", ParametersMode.Expression);
+    public class TimeSpanNotZeroAttribute(
+        [CallerFilePath] string callerFilePath = null,
+        [CallerMemberName] string caller = "")
+        : RuleValueComparisonAttribute($"TimeSpanNotEmpty{caller}{Path.GetFileNameWithoutExtension(callerFilePath)}",
+            DefaultContexts.Save,
+            ValueComparisonType.NotEquals, "#00:00:00#", ParametersMode.Expression);
 }
