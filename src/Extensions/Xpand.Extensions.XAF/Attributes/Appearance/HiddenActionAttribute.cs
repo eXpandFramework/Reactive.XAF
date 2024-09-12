84078416e88934633fc51c107d1f6f425fd59696
apobekiaris <apostolis.bekiaris@gmail.com>
2024-09-12 13:04:59 +0300

    DeactivateDeleteActionAttribute

--- src/Extensions/Xpand.Extensions.XAF/Attributes/Appearance/HiddenActionAttribute.cs
+++ src/Extensions/Xpand.Extensions.XAF/Attributes/Appearance/HiddenActionAttribute.cs
@@ -1,6 +1,7 @@
 ﻿using System;
+using DevExpress.ExpressApp.SystemModule;
 
-namespace Xpand.Extensions.XAF.Attributes{
+namespace Xpand.Extensions.XAF.Attributes.Appearance{
     [AttributeUsage(AttributeTargets.Class|AttributeTargets.Property,AllowMultiple = true)]
     public class HiddenActionAttribute(params string[] actions) : Attribute {
         public string[] Actions{ get; } = actions;
