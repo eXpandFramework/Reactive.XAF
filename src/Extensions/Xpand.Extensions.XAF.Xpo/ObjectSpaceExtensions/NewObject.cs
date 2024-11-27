348f38dc830826b99a5de819f1b1539554c4ed07
apobekiaris <apostolis.bekiaris@gmail.com>
2024-11-27 08:22:21 +0200

    NewObject extension

---
+++ src/Extensions/Xpand.Extensions.XAF.Xpo/ObjectSpaceExtensions/NewObject.cs
@@ -0,0 +1,12 @@
+﻿using DevExpress.ExpressApp;
+using Fasterflect;
+
+namespace Xpand.Extensions.XAF.Xpo.ObjectSpaceExtensions {
+    public static partial class ObjectSpaceExtensions {
+        public static T NewObject<T>(this IObjectSpace objectSpace) where T : IObjectSpaceLink {
+            var instance = (T)typeof(T).CreateInstance(objectSpace.UnitOfWork());
+            instance.ObjectSpace = objectSpace;
+            return instance;
+        }
+    }
+}
