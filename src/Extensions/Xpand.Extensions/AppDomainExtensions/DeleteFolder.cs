0402dda6c1acb081f30ef95e08e91e9ef4e3b119
apobekiaris <apostolis.bekiaris@gmail.com>
2024-12-03 11:28:53 +0200

    DeleteFolder

---
+++ src/Extensions/Xpand.Extensions/AppDomainExtensions/DeleteFolder.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.IO;
+
+namespace Xpand.Extensions.AppDomainExtensions {
+    public static partial class AppDomainExtensions {
+        public static void DeleteFolder(this AppDomain appDomain, string name, bool recursive = true) {
+            var path = $"{appDomain.ApplicationPath()}{name}";
+            if (Directory.Exists(path)) {
+                Directory.Delete(path, recursive);
+            }
+        }
+    }
+}
