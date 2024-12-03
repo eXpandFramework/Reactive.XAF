7cfc720e60b0f6c53c07c123603aae469c0cffd3
apobekiaris <apostolis.bekiaris@gmail.com>
2024-12-03 11:28:18 +0200

    CalculateEditorType

---
+++ src/Extensions/Xpand.Extensions.XAF/ModelExtensions/CalculateEditorType.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using DevExpress.ExpressApp.Editors;
+using DevExpress.ExpressApp.Model;
+
+namespace Xpand.Extensions.XAF.ModelExtensions {
+    public static partial class ModelExtensions {
+        public static Type CalculateEditorType(this IModelMember modelMember)
+            => new MemberEditorInfoCalculator().GetEditorType(modelMember);
+    }
+}
