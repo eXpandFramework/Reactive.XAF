2931a067ef4ea831b363ee727bcd0eacbf47cad3
apobekiaris <apostolis.bekiaris@gmail.com>
2024-12-03 11:27:39 +0200

    VisibleInAllViewsAttribute CreateModelMember

--- src/Extensions/Xpand.Extensions.XAF/Attributes/VisibleInAllViewsAttribute.cs
+++ src/Extensions/Xpand.Extensions.XAF/Attributes/VisibleInAllViewsAttribute.cs
@@ -3,5 +3,11 @@
 namespace Xpand.Extensions.XAF.Attributes {
     [AttributeUsage(AttributeTargets.Property)]
     public class VisibleInAllViewsAttribute:Attribute {
+        public VisibleInAllViewsAttribute(bool createModelMember) => CreateModelMember = createModelMember;
+
+        public VisibleInAllViewsAttribute(){
+        }
+
+        public bool CreateModelMember { get; }
     }
 }
