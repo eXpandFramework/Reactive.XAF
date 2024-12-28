00024cfb2f6d9fe5f1017a0968585cd894e84f05
apobekiaris <apostolis.bekiaris@gmail.com>
2024-12-28 10:54:51 +0200

    HyperLinkPropertyEditorAttribute: ControlClickListView

--- src/Extensions/Xpand.Extensions.XAF/Attributes/HyperLinkPropertyEditorAttribute.cs
+++ src/Extensions/Xpand.Extensions.XAF/Attributes/HyperLinkPropertyEditorAttribute.cs
@@ -4,5 +4,6 @@ namespace Xpand.Extensions.XAF.Attributes {
     [AttributeUsage(AttributeTargets.Property)]
     public class HyperLinkPropertyEditorAttribute(string name) : Attribute {
         public string Name { get; } = name;
+        public bool ControlClickListView { get; set; }
     }
 }
