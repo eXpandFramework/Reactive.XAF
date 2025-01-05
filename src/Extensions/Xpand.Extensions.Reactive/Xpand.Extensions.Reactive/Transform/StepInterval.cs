6d10817b6f52461aea52b329302ce2cabe6b3b27
apobekiaris <apostolis.bekiaris@gmail.com>
2025-01-05 12:02:02 +0200

    StepRandomInterval

--- src/Extensions/Xpand.Extensions.Reactive/Xpand.Extensions.Reactive/Transform/StepInterval.cs
+++ src/Extensions/Xpand.Extensions.Reactive/Xpand.Extensions.Reactive/Transform/StepInterval.cs
@@ -7,5 +7,14 @@ namespace Xpand.Extensions.Reactive.Transform{
     public static partial class Transform{
         public static IObservable<T> StepInterval<T>(this IObservable<T> source, TimeSpan minDelay, IScheduler scheduler = null) 
             => source.Select(x => minDelay.Timer(scheduler??Scheduler.Default).Select(_ => x)).Concat();
+        
+        public static IObservable<T> StepRandomInterval<T>(this IObservable<T> source, TimeSpan minDelay, TimeSpan maxDelay, IScheduler scheduler=null) {
+            var r=new Random();
+            return source.Select(x => {
+                var d=TimeSpan.FromMilliseconds(r.Next((int)minDelay.TotalMilliseconds,(int)maxDelay.TotalMilliseconds));
+                return d.Timer(scheduler??Scheduler.Default).Select(_=>x);
+            }).Concat();
+        }
+
     }
 }
