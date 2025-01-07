c156e884e5c7b1e66cb6349b35d05e99c20b85f3
apobekiaris <apostolis.bekiaris@gmail.com>
2025-01-07 21:14:59 +0200

    FindFreePort extension

--- src/Extensions/Xpand.Extensions/Network/GetRandomAvailablePort.cs
+++ src/Extensions/Xpand.Extensions/Network/GetRandomAvailablePort.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Linq;
 using System.Net;
+using System.Net.Sockets;
 using Xpand.Extensions.Numeric;
 
 namespace Xpand.Extensions.Network {
@@ -9,5 +10,18 @@ namespace Xpand.Extensions.Network {
         public static int GetRandomAvailablePort(this IPEndPoint[] endPoints, int startRange = 1024, int endRange = 49151)
             => startRange.Range(endRange - startRange).ToArray().OrderBy(_ => Random.Next()).ToArray()
                 .First(port => endPoints.All(endPoint => endPoint.Port != port));
+
+        public static int FindFreePort(this AppDomain appDomain) {
+            var socket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+            try {
+                var localIP = new IPEndPoint(IPAddress.Any, 0);
+                socket.Bind(localIP);
+                return ((IPEndPoint) socket.LocalEndPoint ?? throw new Exception("Not found free port.")).Port;
+            }
+            finally {
+                socket.Close();
+            }
+        }
+
     }
 }
