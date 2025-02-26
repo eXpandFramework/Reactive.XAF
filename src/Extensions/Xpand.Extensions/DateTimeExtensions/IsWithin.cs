2621cfc5471a2515d56e760efc4d2eb85d31fd97
apobekiaris <apostolis.bekiaris@gmail.com>
2025-02-26 06:37:23 +0200

    DateTimeExtensions

---
+++ src/Extensions/Xpand.Extensions/DateTimeExtensions/IsWithin.cs
@@ -0,0 +1,18 @@
+using System;
+
+namespace Xpand.Extensions.DateTimeExtensions {
+    public static partial class DateTimeExtensions {
+        public static bool IsWithinTomorrow(this DateTime dt)
+            => !dt.IsWithinADay()&& dt.IsWithinHours(48);
+        
+        public static bool IsWithinADay(this DateTime dt)
+            => dt.IsWithinHours(24);
+        
+        
+        public static bool IsWithinHours(this DateTime date, double hours)
+            => date.IsWithin(System.TimeSpan.FromHours(hours));
+        
+        public static bool IsWithin(this DateTime date, TimeSpan timeSpan)
+            => date >= DateTime.Now - timeSpan && date <= DateTime.Now + timeSpan;
+    }
+}
---
+++ src/Extensions/Xpand.Extensions/DateTimeExtensions/TimeToStart.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace Xpand.Extensions.DateTimeExtensions {
+    public static partial class DateTimeExtensions {
+        public static TimeSpan TimeToPast(this DateTime dateTime)
+            => dateTime > DateTime.Now ? dateTime.Subtract(DateTime.Now) : System.TimeSpan.Zero;
+    }
+}
