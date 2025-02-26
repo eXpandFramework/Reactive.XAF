fbc3e5ad45d1212a3f78931087bf9960eab9fa24
apobekiaris <apostolis.bekiaris@gmail.com>
2025-02-26 06:32:31 +0200

    CompleteOnTimeout

--- src/Extensions/Xpand.Extensions.Reactive/Xpand.Extensions.Reactive/ErrorHandling/CompleteOnError.cs
+++ src/Extensions/Xpand.Extensions.Reactive/Xpand.Extensions.Reactive/ErrorHandling/CompleteOnError.cs
@@ -1,11 +1,12 @@
 ﻿using System;
 using System.Reactive.Linq;
+using System.Runtime.CompilerServices;
 using Xpand.Extensions.Reactive.Transform;
 
 namespace Xpand.Extensions.Reactive.ErrorHandling {
 	public static partial class ErrorHandling {
 		
-		public static IObservable<T> CompleteOnError<T>(this IObservable<T> source,Action<Exception> onError=null,Func<Exception,bool> match=null)
+		public static IObservable<T> CompleteOnError<T>(this IObservable<T> source,Action<Exception> onError=null,Func<Exception,bool> match=null,[CallerMemberName]string caller="")
 			=> source.Catch<T,Exception>(exception => {
 				if (!(match?.Invoke(exception) ?? true)) return exception.Throw<T>();
 				onError?.Invoke(exception);
@@ -17,5 +18,8 @@ namespace Xpand.Extensions.Reactive.ErrorHandling {
         
 		public static IObservable<T> CompleteOnError<T>(this IObservable<T> source,Type exceptionType,Action<Exception> onError=null) 
 			=> source.CompleteOnError(onError,exceptionType.IsInstanceOfType);
+		public static IObservable<T> CompleteOnTimeout<T>(this IObservable<T> source) 
+			=> source.CompleteOnError(typeof(TimeoutException));
+		
 	}
 }
