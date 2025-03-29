95f687a8499125787a29e452fb38c83f8da132ce
apobekiaris <apostolis.bekiaris@gmail.com>
2025-03-29 14:36:02 +0200

    WaitForMainWindowHandle

---
+++ src/Extensions/Xpand.Extensions/ProcessExtensions/WaitForMainWindowHandle.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text.RegularExpressions;
+using System.Threading;
+using Xpand.Extensions.LinqExtensions;
+using Xpand.Extensions.Numeric;
+
+namespace Xpand.Extensions.ProcessExtensions {
+    public static partial class ProcessExtensions {
+        public static IntPtr WaitForMainWindowHandle(this Process process, TimeSpan? timeout = null) {
+            var stopwatch = Stopwatch.StartNew();
+            while (stopwatch.Elapsed < (timeout ?? 5.Seconds())) {
+                if (process.MainWindowHandle != IntPtr.Zero)
+                    return process.MainWindowHandle;
+                Thread.Sleep(200);
+                process.Refresh();
+            }
+
+            return IntPtr.Zero;
+        }
+    }
+}
