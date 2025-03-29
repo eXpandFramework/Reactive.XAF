39d0efe18ff4ae694794a453aa3b0800fa8c1274
apobekiaris <apostolis.bekiaris@gmail.com>
2025-03-29 14:36:32 +0200

    SetCredentials

---
+++ src/Extensions/Xpand.Extensions/UriExtensions/SetCredentials.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Linq;
+using System.Net;
+using Xpand.Extensions.LinqExtensions;
+
+namespace Xpand.Extensions.UriExtensions {
+    public static partial class UriExtensions {
+        public static NetworkCredential SetCredentials(this WebProxy proxy, Uri address) {
+            if (string.IsNullOrEmpty(address.UserInfo)) return null;
+            proxy.UseDefaultCredentials = false;
+            proxy.Address = new Uri(address.Scheme.JoinString("://", address.Host));
+            var strings = address.UserInfo.Split(':');
+            return new NetworkCredential(strings.First(), strings.Last());
+        }
+    }
+}
