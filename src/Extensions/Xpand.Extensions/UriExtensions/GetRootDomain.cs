c9e079ed2e1a821da2269fd0671ae1ba0eb78ffb
apobekiaris <apostolis.bekiaris@gmail.com>
2025-03-29 14:36:16 +0200

    GetRootDomain

---
+++ src/Extensions/Xpand.Extensions/UriExtensions/GetRootDomain.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Linq;
+
+namespace Xpand.Extensions.UriExtensions {
+    public static partial class UriExtensions {
+        public static Uri GetRootUri(this Uri url) => new($"{url.Scheme}://{new Uri(url.WithoutQuery()).GetRootDomain()}");
+
+        public static string GetRootDomain(this Uri url) {
+            var host = url.Host.Split('.');
+            return host.Length < 2
+                ? string.Join(".", host)
+                : string.Join(".", host.Skip(host.Length - 2));
+        }
+        
+        public static string WithoutQuery(this Uri uri) {
+            var s = uri.ToString();
+            var i = s.IndexOf('?');
+            return i >= 0 ? s.Substring(0, i) : s;
+        }
+    }
+}
