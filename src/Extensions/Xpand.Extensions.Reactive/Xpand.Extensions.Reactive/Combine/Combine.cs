abdb453c060aaa6db8767553463bc2c0687df2e4
apobekiaris <apostolis.bekiaris@gmail.com>
2025-05-09 13:43:58 +0300

    simple CombineLatestWhenFirstEmits

--- src/Extensions/Xpand.Extensions.Reactive/Xpand.Extensions.Reactive/Combine/Combine.cs
+++ src/Extensions/Xpand.Extensions.Reactive/Xpand.Extensions.Reactive/Combine/Combine.cs
@@ -22,30 +22,32 @@ namespace Xpand.Extensions.Reactive.Combine {
             => source.Select(t => t.current);
         
         public static IObservable<TResult> CombineLatestWhenFirstEmits<TFirst, TSecond, TResult>(
-            this IObservable<TFirst> first, IObservable<TSecond> second, Func<TFirst, TSecond, TResult> resultSelector)
-            => Observable.Create<TResult>(observer => {
-                TFirst firstValue;
-                var secondValue = default(TSecond);
-                var secondHasValue = false;
+            this IObservable<TFirst> first, IObservable<TSecond> second, Func<TFirst, TSecond, TResult> resultSelector) {
+            return first.WithLatestFrom(second, resultSelector);
+            // return Observable.Create<TResult>(observer => {
+            //     TFirst firstValue;
+            //     var secondValue = default(TSecond);
+            //     var secondHasValue = false;
+            //
+            //     var subscription1 = first.Subscribe(x => {
+            //             firstValue = x;
+            //             _ = true;
+            //             if (secondHasValue) {
+            //                 observer.OnNext(resultSelector(firstValue, secondValue));
+            //             }
+            //         },
+            //         observer.OnError, observer.OnCompleted);
+            //
+            //     var subscription2 = second.Subscribe(x => {
+            //         secondValue = x;
+            //         secondHasValue = true;
+            //     }, observer.OnError, () => { });
+            //
+            //     return new CompositeDisposable(subscription1, subscription2);
+            // });
+        }
 
-                var subscription1 = first.Subscribe(x => {
-                        firstValue = x;
-                        _ = true;
-                        if (secondHasValue) {
-                            observer.OnNext(resultSelector(firstValue, secondValue));
-                        }
-                    },
-                    observer.OnError, observer.OnCompleted);
 
-                var subscription2 = second.Subscribe(x => {
-                    secondValue = x;
-                    secondHasValue = true;
-                }, observer.OnError, () => { });
-
-                return new CompositeDisposable(subscription1, subscription2);
-            });
-    
-        
         public static IObservable<TResult> CombineVeryLatest<TLeft, TRight, TResult>(this IObservable<TLeft> leftSource,
             IObservable<TRight> rightSource, Func<TLeft, TRight, TResult> selector) 
             => Observable.Defer(() => {
