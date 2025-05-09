472da076c3caf72a765e4131a101a7a109a72b3f
apobekiaris <apostolis.bekiaris@gmail.com>
2025-05-09 13:46:43 +0300

    IsBetween

---
+++ src/Extensions/Xpand.Extensions/DateTimeExtensions/IsBetween.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace Xpand.Extensions.DateTimeExtensions {
+    public static partial class DateTimeExtensions {
+        public static bool IsBetween(this DateTime dateTime, DateTime left, DateTime right)
+            => dateTime > left && dateTime < right;
+    }
+}
