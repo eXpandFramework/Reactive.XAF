d76454362cfec47ef837b7b0dafec1dcc25a5127
apobekiaris <apostolis.bekiaris@gmail.com>
2025-05-09 13:46:08 +0300

    GetManifestResourceStreams overload

--- src/Extensions/Xpand.Extensions/AssemblyExtensions/GetManifestResourceStream.cs
+++ src/Extensions/Xpand.Extensions/AssemblyExtensions/GetManifestResourceStream.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Reflection;
@@ -8,6 +9,9 @@ namespace Xpand.Extensions.AssemblyExtensions {
     public static partial class AssemblyExtensions {
         public static Stream GetManifestResourceStream(this Assembly assembly, Func<string, bool> nameMatch)
             => assembly.GetManifestResourceStream(assembly.GetManifestResourceNames().First(nameMatch));
+        public static IEnumerable<Stream> GetManifestResourceStreams(this Assembly assembly, Func<string, bool> nameMatch)
+            => assembly.GetManifestResourceNames().Where(nameMatch).Select(assembly.GetManifestResourceStream);
+        
         public static byte[] GetManifestResourceBytes(this Assembly assembly, Func<string, bool> nameMatch) {
             using var stream = assembly.GetManifestResourceStream(assembly.GetManifestResourceNames().First(nameMatch));
             return stream.Bytes();
