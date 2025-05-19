e90a65d66636c361c370f1d9bd1c1fa197d939db
apobekiaris <apostolis.bekiaris@gmail.com>
2025-05-19 05:36:05 +0300

    ToMinutes, ToSeconds

--- src/Extensions/Xpand.Extensions/Numeric/Seconds.cs
+++ src/Extensions/Xpand.Extensions/Numeric/Seconds.cs
@@ -2,7 +2,9 @@ using System;
 
 namespace Xpand.Extensions.Numeric {
     public static partial class NumericExtensions {
+        public static TimeSpan ToMinutes(this int minutes) => TimeSpan.FromMinutes(minutes);
         public static TimeSpan Minutes(this int minutes) => TimeSpan.FromMinutes(minutes);
+        public static TimeSpan ToSeconds(this int seconds) => TimeSpan.FromSeconds(seconds);
         public static TimeSpan Seconds(this int seconds) => TimeSpan.FromSeconds(seconds);
         public static TimeSpan Seconds(this double seconds) => TimeSpan.FromSeconds(seconds);
         public static TimeSpan Hours(this int hours) => TimeSpan.FromHours(hours);
