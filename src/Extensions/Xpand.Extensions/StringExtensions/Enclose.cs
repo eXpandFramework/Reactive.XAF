c73737f00fd8d840b9eafc67bb2c6e0d14863ca7
apobekiaris <apostolis.bekiaris@gmail.com>
2025-07-02 12:28:28 +0300

    Enclose string extensions

--- src/Extensions/Xpand.Extensions/StringExtensions/Enclose.cs
+++ src/Extensions/Xpand.Extensions/StringExtensions/Enclose.cs
@@ -1,4 +1,5 @@
-﻿using Xpand.Extensions.LinqExtensions;
+﻿using System.Linq;
+using Xpand.Extensions.LinqExtensions;
 
 namespace Xpand.Extensions.StringExtensions {
     public static partial class StringExtensions {
@@ -8,7 +9,23 @@ namespace Xpand.Extensions.StringExtensions {
             end ??= start;
             return input.StartsWith(start) && input.EndsWith(end) ? input : new[] { start, input, end }.Join();
         }
+
+        public static string EncloseHTMLNonImportant(this string input,bool useItalic=false)
+            => input.EncloseHTMLTag(!useItalic?"<span class=\"tg-spoiler\">":"i");
+        public static string EncloseHTMLVeryImportant(this string input)
+            => input.EncloseHTMLTag("b","u");
+        public static string EncloseHTMLImportant(this string input)
+            => input.EncloseHTMLTag("b");
         
+        public static string EncloseHTMLTag(this string input, params string[] tags)
+            => tags.Reverse().Aggregate(input, (current, tag) => {
+                var tagParts = tag.Split(' ', 2);
+                var tagName = tagParts[0];
+                var attributes = tagParts.Length > 1 ? " " + tagParts[1] : "";
+                return $"<{tagName}{attributes}>{current}</{tagName}>";
+            });
+
+
         public static string EncloseParenthesis(this string input) => input.Enclose("(", ")");
     }
 }
