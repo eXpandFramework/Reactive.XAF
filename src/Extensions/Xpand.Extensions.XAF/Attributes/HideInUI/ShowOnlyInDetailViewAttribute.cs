4055610fef5a645ef0fa8e114896d7ffd873e2d9
apobekiaris <apostolis.bekiaris@gmail.com>
2025-07-17 17:10:17 +0300

    ShowOnlyInDetailViewAttribute

---
+++ src/Extensions/Xpand.Extensions.XAF/Attributes/HideInUI/ShowOnlyInDetailViewAttribute.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using DevExpress.Persistent.Base;
+
+namespace Xpand.Extensions.XAF.Attributes.HideInUI;
+[AttributeUsage(AttributeTargets.Property | AttributeTargets.Field)]
+public class ShowOnlyInDetailViewAttribute(bool includeCustomizationForm = true, bool includeEditor = true, bool includeModelMember = true)
+    : HideInUIAttribute(DevExpress.Persistent.Base.HideInUI.All & ~(
+                            DevExpress.Persistent.Base.HideInUI.DetailView | 
+                            (includeCustomizationForm ? DevExpress.Persistent.Base.HideInUI.DetailViewCustomizationForm : 0) | 
+                            (includeEditor ? DevExpress.Persistent.Base.HideInUI.DetailViewEditor : 0) | 
+                            (includeModelMember ? DevExpress.Persistent.Base.HideInUI.ModelMember : 0)
+                        ));
