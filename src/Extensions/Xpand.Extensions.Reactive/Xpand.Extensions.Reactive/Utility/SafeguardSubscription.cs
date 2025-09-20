2c2787eb7ecdb7c77b1f0e6847e80259ef200aa5
apobekiaris <apostolis.bekiaris@gmail.com>
2025-09-20 16:34:57 +0300

    RunSideEffects

--- src/Extensions/Xpand.Extensions.Reactive/Xpand.Extensions.Reactive/Utility/SafeguardSubscription.cs
+++ src/Extensions/Xpand.Extensions.Reactive/Xpand.Extensions.Reactive/Utility/SafeguardSubscription.cs
@@ -2,6 +2,7 @@
 using System.Reactive.Disposables;
 using System.Reactive.Linq;
 using System.Runtime.CompilerServices;
+using Xpand.Extensions.Reactive.Transform;
 
 namespace Xpand.Extensions.Reactive.Utility {
     
@@ -10,13 +11,7 @@ namespace Xpand.Extensions.Reactive.Utility {
         public static IObservable<TResult> Use<T, TResult>(this T source, Func<T, IObservable<TResult>> selector) where T : IDisposable 
             => selector(source).Finally(source.Dispose);
 
-        public static IObservable<T> RunSideEffect<T, TSide>(this IObservable<T> source, IObservable<TSide> sideEffect, bool propagateSideEffectError = false) => source.Publish(bus => {
-            var safeSideEffect = propagateSideEffectError ? sideEffect
-                : sideEffect.OnErrorResumeNext(Observable.Empty<TSide>());
-            return bus.TakeUntil(bus.LastOrDefaultAsync()
-                .Zip(safeSideEffect.LastOrDefaultAsync()));
-        });
-        
+
         public static IObservable<T> SafeguardSubscription<T>(this IObservable<T> source,Action<Exception,string> onError, [CallerMemberName] string caller = "")
             => Observable.Create<T>(observer => {
                 IDisposable subscription;
