0c148b6550808f2922b19ea1c65ac8747692dbd8
apobekiaris <apostolis.bekiaris@gmail.com>
2025-12-14 09:15:26 +0200

    WithEmissionTime

---
+++ src/Extensions/Xpand.Extensions.Reactive/Xpand.Extensions.Reactive/Utility/WithEmissionTime.cs
@@ -0,0 +1,9 @@
+﻿using System;
+using System.Reactive.Linq;
+
+namespace Xpand.Extensions.Reactive.Utility {
+    public static partial class Utility {
+        public static IObservable<(T Value, TimeSpan Duration)> WithEmissionTime<T>(this IObservable<T> source)
+            => source.TimeInterval().Select(i => (i.Value, i.Interval));
+    }
+}
