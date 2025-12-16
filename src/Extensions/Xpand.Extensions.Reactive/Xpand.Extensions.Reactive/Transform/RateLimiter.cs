3b1314541b27250289d7ed8021557b37e45fa4b7
apobekiaris <apostolis.bekiaris@gmail.com>
2025-12-16 07:28:30 +0200

    Ratelimit tests

--- src/Extensions/Xpand.Extensions.Reactive/Xpand.Extensions.Reactive/Transform/RateLimiter.cs
+++ src/Extensions/Xpand.Extensions.Reactive/Xpand.Extensions.Reactive/Transform/RateLimiter.cs
@@ -1,46 +1,44 @@
 ﻿using System;
 using System.Collections.Concurrent;
-using System.Reactive.Concurrency;
 using System.Reactive.Linq;
 using System.Threading;
 using System.Threading.Tasks;
-using Xpand.Extensions.Reactive.Utility;
 
 namespace Xpand.Extensions.Reactive.Transform {
     public static partial class Transform {
         private static readonly ConcurrentDictionary<string, RateLimitingGroup> Groups = new();
 
-        public static IObservable<T> RateLimit<T>(this T source, double emissionsPerSecond, string groupName = nameof(RateLimit))
-            =>emissionsPerSecond==0?source.Observe(): source.Defer(() => {
-                var group = Groups.GetOrAdd(groupName, new RateLimitingGroup(emissionsPerSecond));
-                return Observable.StartAsync(() => group.WaitAsync()).Select(_ => source);
-            });
+        public static IObservable<T> RateLimit<T>(this IObservable<T> source, double emissionsPerSecond,
+            string groupName = nameof(RateLimit))
+            => emissionsPerSecond <= 0 ? source : Observable.Defer(() => {
+                    var group = Groups.GetOrAdd(groupName, static (_, rate) => new RateLimitingGroup(rate), emissionsPerSecond);
+                    return source.Select(item => Observable.FromAsync(group.WaitAsync).Select(_ => item)
+                    ).Concat();
+                });        
         
-        private class RateLimitingGroup {
-            private readonly SemaphoreSlim _semaphore;
-            private readonly double _maxRequestsPerSecond;
-            
-            private DateTimeOffset _lastEmission;
-    
-            public RateLimitingGroup(double maxRequestsPerSecond) {
-                _maxRequestsPerSecond = maxRequestsPerSecond;
-                
-                _semaphore = new SemaphoreSlim(1, 1);
-                _lastEmission = DateTimeOffset.MinValue;
-            }
-    
+        public static IObservable<T> RateLimit<T>(this T source, double emissionsPerSecond, string groupName = nameof(RateLimit))
+            =>source.Observe().RateLimit(emissionsPerSecond, groupName);
+        
+        private class RateLimitingGroup(double maxRequestsPerSecond) {
+            private readonly SemaphoreSlim _semaphore = new(1, 1);
+            private DateTimeOffset _lastEmission = DateTimeOffset.MinValue;
+
             public async Task WaitAsync() {
                 await _semaphore.WaitAsync();
-                var now = DefaultScheduler.Instance.Now;
-                var requiredDelay = _lastEmission + TimeSpan.FromSeconds(1.0 / _maxRequestsPerSecond) - now;
-                if (requiredDelay > TimeSpan.Zero) {
-                    await Task.Delay(requiredDelay, CancellationToken.None);
+                try {
+                    var now = DateTimeOffset.UtcNow;
+                    var requiredDelay = _lastEmission + TimeSpan.FromSeconds(1.0 / maxRequestsPerSecond) - now;
+                    if (requiredDelay > TimeSpan.Zero) {
+                        await Task.Delay(requiredDelay, CancellationToken.None);
+                    }
+                    _lastEmission = DateTimeOffset.UtcNow;
                 }
-    
-                _lastEmission = DefaultScheduler.Instance.Now;
-                _semaphore.Release();
+                finally {
+                    _semaphore.Release();
+                }
             }
         }
-        }
+        
+    }
     }
     
---
+++ src/Tests/Extensions/RateLimitTests.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Diagnostics;
+using System.Reactive.Linq;
+using System.Reactive.Threading.Tasks;
+using System.Threading.Tasks;
+using NUnit.Framework;
+using Shouldly;
+using Xpand.Extensions.Reactive.Transform;
+using Xpand.TestsLib;
+
+namespace Xpand.Extensions.Tests {
+    public class RateLimitTests : BaseTest {
+        [Test]
+        public async Task RateLimit_Should_Throttle_Emissions() {
+            const int count = 5;
+            const double ratePerSecond = 10;
+            var expectedDuration = TimeSpan.FromSeconds((count - 1) / ratePerSecond);
+
+            var stopwatch = Stopwatch.StartNew();
+
+            await Observable.Range(1, count)
+                .RateLimit(ratePerSecond, Guid.NewGuid().ToString())
+                .Buffer(count);
+
+            stopwatch.Stop();
+
+            stopwatch.Elapsed.ShouldBeGreaterThanOrEqualTo(expectedDuration);
+            stopwatch.Elapsed.TotalSeconds.ShouldBeLessThan(expectedDuration.TotalSeconds + 0.5);
+        }
+
+        [Test]
+        public async Task RateLimit_Should_Isolate_Different_Groups() {
+            const double ratePerSecond = 1;
+            var groupA = Guid.NewGuid().ToString();
+            var groupB = Guid.NewGuid().ToString();
+
+            var stopwatch = Stopwatch.StartNew();
+
+            var taskA = Observable.Return(1).Repeat(2).RateLimit(ratePerSecond, groupA).ToTask();
+            var taskB = Observable.Return(1).Repeat(2).RateLimit(ratePerSecond, groupB).ToTask();
+
+            await Task.WhenAll(taskA, taskB);
+
+            stopwatch.Stop();
+
+            stopwatch.Elapsed.TotalSeconds.ShouldBeInRange(0.9, 1.9);
+        }
+
+        [Test]
+        public async Task RateLimit_Should_Share_Limit_Across_Subscriptions_Same_Group() {
+            const double ratePerSecond = 2;
+            var group = Guid.NewGuid().ToString();
+
+            var stopwatch = Stopwatch.StartNew();
+
+            await Observable.Return(1).RateLimit(ratePerSecond, group);
+
+            await Observable.Return(1).RateLimit(ratePerSecond, group);
+
+            stopwatch.Stop();
+
+            stopwatch.Elapsed.TotalMilliseconds.ShouldBeGreaterThan(450);
+        }
+
+        [Test]
+        public async Task RateLimit_Should_PassThrough_When_Rate_Is_Zero() {
+            var stopwatch = Stopwatch.StartNew();
+
+            await Observable.Range(1, 100)
+                .RateLimit(0, Guid.NewGuid().ToString())
+                .Buffer(100);
+
+            stopwatch.Stop();
+
+            stopwatch.Elapsed.TotalMilliseconds.ShouldBeLessThan(100);
+        }
+    }
+}
