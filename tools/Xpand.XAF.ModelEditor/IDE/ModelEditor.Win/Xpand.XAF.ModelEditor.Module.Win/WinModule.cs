2b7ff48b1666a458a3ee227baf1ef05aa461fc9c
apobekiaris <apostolis.bekiaris@gmail.com>
2026-06-27 06:40:56 +0300

    Update Xpand ME

--- tools/Xpand.XAF.ModelEditor/IDE/ModelEditor.Win/Xpand.XAF.ModelEditor.Module.Win/WinModule.cs
+++ tools/Xpand.XAF.ModelEditor/IDE/ModelEditor.Win/Xpand.XAF.ModelEditor.Module.Win/WinModule.cs
@@ -1,4 +1,5 @@
 ﻿using System.ComponentModel;
+using System.Diagnostics;
 using DevExpress.ExpressApp;
 using Xpand.XAF.Modules.ModelMapper.Configuration;
 using Xpand.XAF.Modules.ModelMapper.Services;
